6c1f36ad85c61db1564c3ddc5de483e63c634d30
Nayomal Lakshitha <nmoragane@gmail.com>
2021-01-15 23:07:40 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbZf8c8TA0fLrhaoGIWPTkuCFAIiY3CrI",
+    authDomain: "whatsapp-mern-cad38.firebaseapp.com",
+    projectId: "whatsapp-mern-cad38",
+    storageBucket: "whatsapp-mern-cad38.appspot.com",
+    messagingSenderId: "554523241856",
+    appId: "1:554523241856:web:6ed13bd18cbdcbc6a9a565"
+  };
